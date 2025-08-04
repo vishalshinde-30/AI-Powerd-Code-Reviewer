341151d2d472b4ac4fb15aedad2fcbad9276a130
vishalshinde-30 <vishal.shinde4795@gmail.com>
2025-08-04 19:58:49 +0530

    Update App.jsx

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://ai-powerd-code-reviewer-backend-kkwc.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
