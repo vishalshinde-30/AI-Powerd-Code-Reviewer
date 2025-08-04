2f676e251da1d342a795f686825ef230854b980a
vishalshinde-30 <vishal.shinde4795@gmail.com>
2025-08-04 20:00:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ const app = require('./src/app')
 
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+    console.log('Server is running on https://ai-powerd-code-reviewer-backend-kkwc.onrender.com')
 })
